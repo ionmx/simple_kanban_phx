78c9ba670b40e900bb95f5a6dfd184aec86c0a2d
Jonathan Hernández <jonathan.hernandez@gmail.com>
2022-06-15 22:23:55 -0600

    Config CORS. Allow traffic from all sources (bad practice)

--- lib/simple_kanban_phx_web/endpoint.ex
+++ lib/simple_kanban_phx_web/endpoint.ex
@@ -44,5 +44,6 @@ defmodule SimpleKanbanPhxWeb.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
+  plug CORSPlug, origin: "*"
   plug SimpleKanbanPhxWeb.Router
 end
